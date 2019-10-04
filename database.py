63efe3dce63520a3ca2df8ead54d90f91c5946b7
smg7d <shane.m.gallagher@gmail.com>
2019-10-04 19:50:55 -0400

    initial creation

---
+++ database.py
@@ -0,0 +1,28 @@
+def createRecord(platform, link, username, password):
+    #generate an id
+    #generate sql query
+    #execute query
+
+    return True
+    
+def read(id):
+    result = {}
+    #generate sql query
+    #execute and put in dictionary 'result'
+
+    return result
+
+def updateRecord(id, col, val):
+    #this function updates a record given all the parameters
+    #create query string dynamically
+    query_string = f'update ({col}) where id = {id} VALUES (val);'
+    #execute query
+
+    return true
+
+def deleteRecord(id):
+    #this function updates a record given all the parameters
+    #create query string dynamically
+    #execute query
+
+    return True
---
+++ interface.py
@@ -0,0 +1,5 @@
+#import tkinter as tk
+#from database import *
+
+#create userform1
+#create userform2
